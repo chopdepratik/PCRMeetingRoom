396f524f0dc3bcb39aef9d72bf1fd0228de42dca
Pratik Chopde <chopdepratik60@gmail.com>
2025-04-21 20:21:45 +0530

    Update VideoCall.jsx
    edit the backend url

--- client/src/components/VideoCall.jsx
+++ client/src/components/VideoCall.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useParams } from 'react-router-dom';
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("enchanting-cranachan-518bec");
 
 const VideoCall = () => {
   const { roomId } = useParams();
